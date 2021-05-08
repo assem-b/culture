f2a237565d28724d71985a390edee957b1d3f26f
Bassem <bassem-dev@posteo.net>
2021-05-08 02:14:48 +0200

    design evolution

---
+++ src/static/js/css.js
@@ -0,0 +1,14 @@
+const billet = document.querySelector('.billet')
+const navbar = document.querySelector('.navbar')
+const message = document.querySelector('#message')
+
+// billet.addEventListener("mouseover", (e) => {
+//     body.style.backgroundColor = "#1C1B29";
+//     navbar.style.color = "white";
+//     message.style.color = "#1C1B29";
+// }, true)
+
+// body.addEventListener("mouseover", (e) => {
+//     body.style.backgroundColor = "white";
+//     navbar.style.color = "black"
+// }, true)
